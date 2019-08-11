fbfc5615348b4eb11e499824ea76f82629fb9e0e
Sky JIA <me@skyjia.com>
2019-08-11 11:07:26 +0800

    remove unnecessary alias

--- shared_profile/.shared_profile_darwin.sh
+++ shared_profile/.shared_profile_darwin.sh
@@ -9,9 +9,6 @@ export SS_SOCKS_PROXY="socks5://127.0.0.1:1086"
 
 alias setproxy="export ALL_PROXY=$SS_SOCKS_PROXY; export http_proxy=$SS_HTTP_PROXY; export https_proxy=$SS_HTTP_PROXY"
 alias unsetproxy="unset ALL_PROXY && unset http_proxy && unset https_proxy"
-alias gitproxy="git config --global http.proxy $SS_HTTP_PROXY"
-alias unsetgitproxy="git config --global --unset http.proxy"
-alias myip="curl -i http://ip.cn"
 
 toggle_proxy() {
     if [ -z ${http_proxy+x} ]; then
