ae7950c1029b77472ddcc120dbd1c6802a4b5653
Sky JIA <me@skyjia.com>
2020-02-25 11:13:39 +0800

    update script

--- update.sh
+++ update.sh
@@ -6,8 +6,14 @@ cd $CUR
 rake
 
 echo "Updating additional dependencies"
+
+echo "Updating zsh"
 git -C $ZSH pull
+
+echo "Updating powerlevel10k"
 ZSH_CUSTOM=$ZSH/custom
 git -C $ZSH_CUSTOM/themes/powerlevel10k pull
+
+echo "Updating SpaceVim"
 git -C $HOME/.SpaceVim pull
 
