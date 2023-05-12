81e73cf9d2ec7db10a0ab5c400ece3c0faa7d07a
Sky Jia <me@skyjia.com>
2023-05-12 11:18:24 +0800

    clear zsh profiles.

--- shared_profile_legacy/.shared_profile_linux.sh
+++
@@ -1,4 +0,0 @@
-# Linux Brew
-export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"
-export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"
