87fd6e82014d75ea868d2bbfc61de48554cbbe5d
Sky Jia <me@skyjia.com>
2025-07-23 19:19:35 +0800

    feat(fish): update docker completions and add just command completions

--- fish/.config/fish/conf.d/claude-code.anyrouter.fish
+++ fish/.config/fish/conf.d/claude-code.anyrouter.fish
@@ -1,5 +1,9 @@
 # Local claude
-alias claude="$HOME/.claude/local/claude"
+set -l clause_local_path "$HOME/.claude/local"
+
+if test -d $clause_local_path
+    ensure_add_to_fish_user_paths "$clause_local_path"
+end
 
 # Use AnyRouter for Claude Code
 set -gx ANTHROPIC_BASE_URL "https://anyrouter.top"
