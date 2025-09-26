bf44bfc4dc7737c902f160ee07feb19cd27c00b4
Sky Jia <me@skyjia.com>
2025-09-26 22:21:52 +0800

    chore: update dependencies in lazy-lock.json and .crates.toml; fix docker completion command and update just-lsp version

--- fish/.config/fish/conf.d/docker.fish
+++ fish/.config/fish/conf.d/docker.fish
@@ -12,8 +12,8 @@ set -l docker_completion_file_path $__fish_config_dir/completions/docker.fish
 if test -f $docker_completion_file_path
     set -l seconds_since_modified (file.seconds_since_modified $docker_completion_file_path)
     if test $seconds_since_modified -ge 604800 # 604800 seconds = 7 days
-        command just --completions fish > $docker_completion_file_path
+        command docker completion fish > $docker_completion_file_path
     end
 else
-  command just --completions fish > $docker_completion_file_path
+  command docker completion fish > $docker_completion_file_path
 end
