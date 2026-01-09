af02970ec221d6e06f67a1bcd08945c5482cfb69
Sky Jia <me@skyjia.com>
2026-01-09 12:33:59 +0800

    chore: remove obsolete Brewfile and related configuration files

--- brew/scripts/brew-dump.sh
+++
@@ -1,6 +0,0 @@
-# dump global Brewfile
-brew bundle dump --global --force --describe --no-vscode --no-go --no-cargo
-
-# dump main Brewfile via rcmdnk/file/brew-file
-# export HOMEBREW_BREWFILE_ON_REQUEST=1
-# brew file init --force
