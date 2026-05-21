97a27d91c26453150175bd51ec712babc38fc44c
Sky Jia <me@skyjia.com>
2026-05-21 11:44:42 +0800

    feat: update .tool-versions for nodejs and ruby versions; refresh lazy-lock.json dependencies; enhance moonbit.fish completions with version option; add user key bindings in config.fish; update Rust crate versions in .crates.toml and .crates2.json; modify nord flavor for symlink color

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,4 +5,16 @@ if status is-interactive
     # set -g fish_greeting
 
     fish_vi_key_bindings
+
+    function fish_user_key_bindings
+        # Customize key bindings to retain Emacs shortcuts in insert mode
+        bind -M insert \ca beginning-of-line    # Ctrl+A go to the beginning of the line
+        bind -M insert \ce end-of-line          # Ctrl+E go to the end of the line
+        bind -M insert \cf forward-char         # Ctrl+F move forward a character
+        bind -M insert \cb backward-char        # Ctrl+B move backward a character
+        bind -M insert \ck kill-line            # Ctrl+K kill the line from the cursor to the end
+        bind -M insert \cu backward-kill-line   # Ctrl+U kill the line from the beginning to the cursor
+        bind -M insert \cw backward-kill-word   # Ctrl+W kill the word before the cursor
+        bind -M insert \cy yank                 # Ctrl+Y yank the last killed text
+    end
 end
