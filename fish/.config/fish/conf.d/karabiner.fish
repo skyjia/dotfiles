32abac5f4cc86a2a4e5c9f7fe31495a2538219e9
Sky Jia <me@skyjia.com>
2026-07-27 14:00:10 +0800

    fix(karabiner): update profile names and aliases for clarity

--- fish/.config/fish/conf.d/karabiner.fish
+++ fish/.config/fish/conf.d/karabiner.fish
@@ -2,17 +2,11 @@
 # https://karabiner-elements.pqrs.org/docs/manual/misc/command-line-interface/
 alias karabiner_cli='/Library/Application\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli'
 
-set -l default_profile "Sky"
-set -l clear_profile "Sky - Clear"
-
-# karabiner_cli --select-profile 'Sky'
-alias karabiner_default_profile="karabiner_cli --select-profile \"$default_profile\""
-
-# karabiner_cli --select-profile 'Sky - Clear'
-alias karabiner_clear_profile="karabiner_cli --select-profile \"$clear_profile\""
-
-# karabiner_cli --show-current-profile-name
 alias karabiner_current_profile="karabiner_cli --show-current-profile-name"
-
-# karabiner_cli --list-profile-names
 alias karabiner_profiles="karabiner_cli --list-profile-names"
+
+set -l personal_profile "Sky"
+set -l clean_profile "Default Clean"
+
+alias karabiner_profile_personal="karabiner_cli --select-profile \"$personal_profile\""
+alias karabiner_profile_clean="karabiner_cli --select-profile \"$clean_profile\""
