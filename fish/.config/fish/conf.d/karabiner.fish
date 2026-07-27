901d034591bb4d8c44622253449579e5ab7a9017
Sky Jia <me@skyjia.com>
2026-07-27 13:35:58 +0800

    fix(fish): add configuration for Karabiner-Elements CLI fix(dependencies): update libuv, lmdb, matplotlib-inline, nh3, psutil, python-build, and fastapi versions fix(nvim): update catppuccin and package-info.nvim commit references in lazy-lock.json fix(submodules): update tmux submodule commit reference

---
+++ fish/.config/fish/conf.d/karabiner.fish
@@ -0,0 +1,3 @@
+# This file is part of the fish shell configuration for Karabiner-Elements.
+# https://karabiner-elements.pqrs.org/docs/manual/misc/command-line-interface/
+alias karabiner_cli='/Library/Application\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli'
